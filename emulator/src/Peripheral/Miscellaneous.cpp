ae3f7f18acba8cb6f46bba1af177a98bfed852dc
LBPHacker <lbphacker@gmail.com>
2017-09-20 14:08:52 +0200

    Ignore all uninteresting SFRs

--- emulator/src/Peripheral/Miscellaneous.cpp
+++ emulator/src/Peripheral/Miscellaneous.cpp
@@ -15,9 +15,12 @@ namespace casioemu
 			((Miscellaneous *)region->userdata)->emulator.chipset.cpu.reg_dsr = data;
 		}, emulator);
 
-		region_F048.Setup(0xF048, 8, "Miscellaneous/F048*8", &data_F048, MMURegion::DefaultRead<uint64_t>, MMURegion::DefaultWrite<uint64_t>, emulator);
-
-		region_F220.Setup(0xF220, 4, "Miscellaneous/F220*4", &data_F220, MMURegion::DefaultRead<uint32_t>, MMURegion::DefaultWrite<uint32_t>, emulator);
+		region_F048.Setup(0xF048, 8, "Miscellaneous/Unknown/F048*8", &data_F048, MMURegion::DefaultRead<uint64_t>, MMURegion::DefaultWrite<uint64_t>, emulator);
+		region_F220.Setup(0xF220, 4, "Miscellaneous/Unknown/F220*4", &data_F220, MMURegion::DefaultRead<uint32_t>, MMURegion::DefaultWrite<uint32_t>, emulator);
+		region_F033.Setup(0xF033, 1, "Miscellaneous/Unknown/F033*1", &data_F033, MMURegion::DefaultRead<uint8_t>, MMURegion::DefaultWrite<uint8_t>, emulator);
+		region_F034.Setup(0xF034, 1, "Miscellaneous/Unknown/F034*1", &data_F034, MMURegion::DefaultRead<uint8_t>, MMURegion::DefaultWrite<uint8_t>, emulator);
+		region_F041.Setup(0xF041, 1, "Miscellaneous/Unknown/F041*1", &data_F041, MMURegion::DefaultRead<uint8_t>, MMURegion::DefaultWrite<uint8_t>, emulator);
+		region_F042.Setup(0xF042, 1, "Miscellaneous/Unknown/F042*1", &data_F042, MMURegion::DefaultRead<uint8_t>, MMURegion::DefaultWrite<uint8_t>, emulator);
 	}
 
 	void Miscellaneous::Uninitialise()
--- emulator/src/Peripheral/Miscellaneous.hpp
+++ emulator/src/Peripheral/Miscellaneous.hpp
@@ -12,6 +12,9 @@ namespace casioemu
 		uint64_t data_F048;
 		uint32_t data_F220;
 
+		MMURegion region_F041, region_F042, region_F033, region_F034;
+		uint8_t data_F041, data_F042, data_F033, data_F034;
+
 	public:
 		using Peripheral::Peripheral;
 
